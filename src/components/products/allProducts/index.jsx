a05ec86eb7d0bb4dfb410e6d5858e3abec52779e
CJ Fulton <fulchr3356@gmail.com>
2021-03-04 18:24:25 -0600

    connected views to backend

--- src/components/products/allProducts/index.jsx
+++ src/components/products/allProducts/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import { Button, ButtonGroup, Container, Table, ListGroup, ListGroupItem} from 'react-bootstrap';
 import guitar from '../product_images/ORANGEWOOD_REY_MOHOGANY_CUTAWAY_BEGINNER_ACOUSTIC_GUITAR-1_2000x.png';
-
+import axios from 'axios';
 
 class AllProducts extends React.Component {
     
@@ -10,14 +10,22 @@ class AllProducts extends React.Component {
 
     constructor(props) {
         super();
-        this.state = {products: 
-            [{  id: '1', 
-                name: 'Guitar',
-                price: '3000.99',
-                description:'A really nice guitar'  
-               } ]};
+        this.state = {products: [],
+            isLoading: false};
         
       }
+
+      componentDidMount() {
+        this.setState({isLoading: true});
+        axios.get(`http://localhost:8080/api/product`)
+        .then(res => {
+          const products = res.data;
+          this.setState({products: products, isLoading: false });
+        })
+      }
+
+
+
 
       async  addToCart(id){
 
@@ -40,7 +48,7 @@ class AllProducts extends React.Component {
       {products.map(product =>
              <tr key={product.id}> 
              <td> <img src = {guitar} width="100" height="50"/></td>
-               <td>{product.name}</td>  
+               <td>{product.productName}</td>  
                <td>${product.price}</td> 
                <td>{product.description}</td>
                <td>
